f99806ba84da49bc390c8d4138fd128821afdca9
limjuchan <sa0466@nate.com>
2018-06-15 11:06:04 +0900

    2018-06-15*1

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,10 +7,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: '임주찬의 포트폴리오' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css' }
     ]
   },
   /*
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,14 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
+<template> 
+   <section class="hero is-primary">
+  <div class="hero-body">
+    <div class="container">
       <h1 class="title">
-        mycloud3
+       임주찬의 포트폴리오
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
-  </section>
-</template>
-
+  </div>
+</section>
+</template> 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
 
