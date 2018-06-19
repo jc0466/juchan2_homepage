b458fc93ce43f17e5c3730b5e92f8c967ebda5aa
limjuchan <sa0466@nate.com>
2018-06-19 15:14:59 +0900

    2018-06-15*9

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <nuxt/>
    <br>
    <br>
-    <footer> 		       	
+    <footer class="footer has-text-centered"> 		       	
 		<p>목원대학교 공대 5층 514호 융합캡스톤디자인실 
     <b>전화:</b> 010-5057-9687</p>
         	<p><address>관리자 : sa0466@nate.com</address>
